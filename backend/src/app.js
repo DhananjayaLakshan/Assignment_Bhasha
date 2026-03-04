09e69420ba52be162bf0a6285a9685e4a99b906b
DhananjayaLakshan <abey.dhananjaya@gmail.com>
2026-03-04 10:29:25 +0530

    initial project structure

---
+++ backend/src/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const notFound = require("./middleware/notFound");
+const errorHandler = require("./middleware/error");
+
+const authRoutes = require("./modules/auth/auth.routes");
+const studentRoutes = require("./modules/students/student.routes");
+const courseRoutes = require("./modules/courses/course.routes");
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/students", studentRoutes);
+app.use("/api/courses", courseRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = app;
