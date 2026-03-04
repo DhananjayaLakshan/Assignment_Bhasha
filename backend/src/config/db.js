c6c45a673a1a0dfdcd023d3d07927781247c6269
DhananjayaLakshan <abey.dhananjaya@gmail.com>
2026-03-04 10:43:48 +0530

    connect to DB

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB Connected");
-    } catch (error) {
-        console.error("MongoDB connection failed:", error.message);
-        process.exit(1);
-    }
-};
+async function connectDB() {
+    const uri = process.env.MONGO_URI;
+    if (!uri) throw new Error('MONGO_URI is not defined in environment variables');
+
+    await mongoose.connect(uri);
+    console.log('Connected to MongoDB');
+}
 
 module.exports = connectDB;
